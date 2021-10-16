9f45dd0add9dcbf747cb8dbd100904b92b9fc236
mahdideveloper <pythonbestir@gmail.com>
2021-10-16 12:09:54 +0330

    First commit

---
+++ food/urls.py
@@ -0,0 +1,33 @@
+"""food URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path, re_path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('store.urls'),name='app_store'),
+    path('',include('accounts.urls'),name='auth'),
+    path('',include('store.api.urls'),name='api'),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
+    re_path('^oauth/', include('social_django.urls', namespace='social')),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
